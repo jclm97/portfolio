9a541b0bd7bc5365c43bf64a581ecdb01e99a7e3
shassen14 <samirhassen14@gmail.com>
2024-10-26 13:37:59 -0500

    footnote update with links

--- src/components/Footnote.tsx
+++ src/components/Footnote.tsx
@@ -2,16 +2,42 @@ const Footnote = () => {
   return (
     <div id="footnote" className="flex mt-10 mb-16">
       <p className="text-muted-foreground">
-        Inspired by{" "}
         <a
-          className="text-foreground"
+          className="text-foreground hover:text-link"
           href="https://brittanychiang.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Brittany Chiang&apos;s website
+        </a>{" "}
+        was my inspiration.{" "}
+        <a
+          className="text-foreground hover:text-link"
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Next.js
         </a>
-        . To build this website, I used Next.js, Tailwind CSS, and Shadcn.
+        ,{" "}
+        <a
+          className="text-foreground hover:text-link"
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tailwind CSS
+        </a>
+        , and{" "}
+        <a
+          className="text-foreground hover:text-link"
+          href="https://ui.shadcn.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          shadcn/ui
+        </a>{" "}
+        were used to build this portfolio website.
       </p>
     </div>
   );
